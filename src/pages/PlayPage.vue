2f00a937965034bad3aa986570b46d434d79839f
s-asai <s-asai@ruby-dev.jp>
2023-01-03 13:26:58 +0900

    テトリミノを落下させる処理の実装

--- src/pages/PlayPage.vue
+++ src/pages/PlayPage.vue
@@ -1,6 +1,45 @@
 <script lang="ts" setup>
-  const classBlockColor = (x: number, y: number): string => {
+  import { reactive } from "vue";
+  import { Tetromino, TETROMINO_TYPE } from '../common/Tetromino';
+  import { Field } from '../common/Field';
+
+  let staticField = new Field();
+  const tetris = reactive({
+    field: new Field(),
+  });
+  const tetromino = reactive({
+    current: Tetromino.random(),
+    position: { x: 3, y: 0 },
+  });
+
+  const classBlockColor = (_x: number, _y: number): string => {
+   const type = tetris.field.data[_y][_x];
+   if(type > 0) {
+     return Tetromino.id(type as TETROMINO_TYPE);
+   }
+
+   const { x, y } = tetromino.position;
+   const { data } = tetromino.current;
+ 
+   if (y <= _y && _y < y + data.length) {
+     const cols = data[_y - y];
+     if (x <= _x && _x < x + cols.length) {
+       if (cols[_x - x] > 0) {
+         return Tetromino.id(cols[_x - x] as TETROMINO_TYPE);
+       }
+     }
+   }
+
+   return "";
   }
+
+  setInterval(() => {
+    tetris.field = Field.deepCopy(staticField);
+  
+    tetromino.position.y++;
+    tetris.field.update(tetromino.current.data, tetromino.position);
+  }, 1 * 1000);
+  tetris.field.update(tetromino.current.data, tetromino.position);
 </script>
 
 <template>
@@ -9,9 +48,7 @@
   <h2>ユーザー名: </h2>
   <div class="container">
     <table class="field" style="border-collapse: collapse">
-      <tr
-        v-for="(row, y) in field"
-        :key="y">
+      <tr v-for="(row, y) in tetris.field.data" :key="y">
         <td
           class="block"
           v-for="(col, x) in row"
